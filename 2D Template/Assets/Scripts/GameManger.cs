d19856ecd2f8627cb9d6e32b1af12e77d0ec0e6e
MUKILTEO\1630107 <160107@mukilteo.wednet.edu>
2025-01-07 08:02:31 -0800

    no message

--- 2D Template/Assets/Scripts/GameManger.cs
+++ 2D Template/Assets/Scripts/GameManger.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class GameManger : MonoBehaviour
 {
     public GameObject gameOverUI;
     // Start is called before the first frame update
