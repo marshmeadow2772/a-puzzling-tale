d3954737ab41fe928655e8907654d56cccc9c41a
Meadow <mphousego@gmail.com>
2025-01-14 23:23:53 -0800

    blah
    blahblahblah

--- 2D Template/Assets/Scripts/Cross.cs
+++ 2D Template/Assets/Scripts/Cross.cs
@@ -1,7 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor.Rendering;
 using UnityEngine;
 
 public class Cross : MonoBehaviour
@@ -10,17 +7,44 @@ public class Cross : MonoBehaviour
     public Transform shootingPoint;
     private GameObject crossInstant;
     public Movement movement;
-    
+
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            
             crossInstant = Instantiate(cross, shootingPoint.position, transform.rotation);
         }
         else if (Input.GetMouseButtonUp(0))
         {
             Destroy(crossInstant);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the object collided with is the "Doverhare"
+        if (other.CompareTag("Doverhare"))
+        {
+            StartCoroutine(StunTarget(other.gameObject));
+        }
+    }
+
+    private IEnumerator StunTarget(GameObject target)
+    {
+        // Assuming the "Doverhare" has a script with a method or property to disable its behavior
+        var targetMovement = target.GetComponent<Movement>();
+        if (targetMovement != null)
+        {
+            targetMovement.enabled = false; // Disable movement or actions
+        }
+
+        // Wait for 10 seconds
+        yield return new WaitForSeconds(10f);
+
+        // Re-enable the "Doverhare's" behavior
+        if (targetMovement != null)
+        {
+            targetMovement.enabled = true;
         }
     }
 }
